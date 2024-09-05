35049bf20458bd7dd8e0feae1d7ee0f6394f4271
Sultana Khan <77052086+SultanaKhan0304@users.noreply.github.com>
2024-09-06 01:56:46 +0530

    PythonBasics6.py

---
+++ PythonBasics6.py
@@ -0,0 +1,24 @@
+#Session 6
+#Functions
+#Example1
+def myfun():
+    print("hello")
+myfun() #Calling the function
+#Example2 - Passing arguments in the function
+def myfun(name):
+    print("Hello",name)
+myfun("Sultana")
+#Example 3
+def cal(a,b):
+    return(a+b)
+sum=cal(10,20)
+print(sum)
+print(cal(10,20)) #alternate method
+#Example 4 
+def func():
+    return
+func() #None
+#Example 5:
+def cal(a,b):
+    print(a+b)
+  cal(10,20)
