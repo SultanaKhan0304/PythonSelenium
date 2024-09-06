6ed206cf7285fe8c7b7d8b32e85fa347db815ad7
Sultana Khan <77052086+SultanaKhan0304@users.noreply.github.com>
2024-09-07 01:07:24 +0530

    PythonBasics8.py

--- PythonBasics8.py
+++ PythonBasics8.py
@@ -52,4 +52,56 @@ class B(A):
 bobj=B()
 bobj.m1()
 bobj.m2()
-
+#Example 3 Multilevel inheritance
+class A:
+    x,y=10,20
+    def m1(self):
+        print(self.x+self.y)
+class B(A):
+    a,b=100,200
+    def m2(self):
+        print(self.a-self.b)
+class C(B):
+    i,j=5,2
+    def m3(self):
+        print(self.i*self.j)
+cobj=C() # Object of C will be able to access m1, m2,m3 
+cobj.m1()
+cobj.m2()
+cobj.m3()
+#Example 4 Hierarchy Inheritance
+class A:
+    x,y=10,20
+    def m1(self):
+        print(self.x+self.y)
+class B(A):
+    a,b=100,200
+    def m2(self):
+        print(self.a-self.b)
+class C(A):
+    i,j=5,2
+    def m3(self):
+        print(self.i*self.j)
+bobj=B() # can only access Class A and Class B
+bobj.m1()
+bobj.m2()
+cobj=C() # can only access class A aand Class C
+cobj.m1()
+cobj.m3()
+#Example 5 Multiple Inheritance - 2 parents for 1 child class
+class A:
+    x,y=10,20
+    def m1(self):
+        print(self.x+self.y)
+class B:
+    a,b=100,200
+    def m2(self):
+        print(self.a-self.b)
+class C(A,B): #Child of Class A and Class B
+    i,j=5,2
+    def m3(self):
+        print(self.i*self.j)
+cobj=C() # can  access class A , Class B, Class C
+cobj.m1()
+cobj.m2()
+cobj.m3()
