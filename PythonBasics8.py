9f54f46cdab40571bdb70d7531f0733bf44fd9ed
Sultana Khan <77052086+SultanaKhan0304@users.noreply.github.com>
2024-09-07 00:52:03 +0530

    PythonBasics8.py

--- PythonBasics8.py
+++ PythonBasics8.py
@@ -26,7 +26,30 @@ class Emp:
         self.sal=sal
     def __str__(self): #Type of constructor to return string type of value
         return(self.ename)
-
 e1=Emp(101,"John",5000)
 print(e1)
-        
+#Inheritance
+#Example1 
+class A:
+    def m1(self):
+        print("this is m1 method from class A")
+class B(A): # B is child class of A
+    def m2(self):
+        print("this is m2 method from class B")
+bobj=B()
+bobj. m1() #m1 is a method of class A , but acessible from Class B as it is the child
+bobj.m2()
+#Example 2 Single Inheritance
+class A:
+    x,y=10,20
+    def m1(self):
+        print(self.x+self.y)
+
+class B(A):
+    a,b=100,200
+    def m2(self):
+        print(self.a-self.b)
+bobj=B()
+bobj.m1()
+bobj.m2()
+
