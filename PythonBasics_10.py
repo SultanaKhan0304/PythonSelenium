e2d9a2b300b7313df9e6dd466282dd25983d2e5a
Sultana Khan <77052086+SultanaKhan0304@users.noreply.github.com>
2024-09-07 23:57:41 +0530

    PythonBasics_10.py

--- PythonBasics_10.py
+++ PythonBasics_10.py
@@ -1,2 +1,26 @@
 #Session 10 Exception Handeling and File Handeling
 #exception Handeling
+#Example 1
+print("This is starting point of program")
+print("This is starting point of program")
+print("This is starting point of program")
+print("This is starting point of program")
+try:
+    print(x)
+except:
+    print("Exception occured")
+    
+print("This is end of program")
+print("This is end of program")
+print("This is end of program")
+print("This is end of program") 
+#Session 10 Exception Handeling and File Handeling
+#exception Handeling
+#Example 2
+print("This is the starting point of program..")
+print("Program in progress")
+try:    
+    print(10/0)
+except ZeroDivisionError:
+    print("Program completed..") 
+print("Program completed")
