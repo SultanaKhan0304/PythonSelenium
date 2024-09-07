ae16a9cde71772475d3764c789a514c25f4bfa7a
Sultana Khan <77052086+SultanaKhan0304@users.noreply.github.com>
2024-09-08 00:49:32 +0530

    PythonBasics_10.py

--- PythonBasics_10.py
+++ PythonBasics_10.py
@@ -24,3 +24,36 @@ try:
 except ZeroDivisionError:
     print("Program completed..") 
 print("Program completed")
+# Example 3 :Multiple except blocks - try,except else, finally
+try:
+    num1,num2=10,5
+    result=num1/num2
+    print("result is",result)
+except ZeroDivisionError:
+    print("Thrown ZeroDivisionError Exception")
+except SyntaxError:
+    print("Thrown Syntax Error exception")
+except:
+    print("Exception handled")
+else:
+    print("No exceptions occured")
+finally:
+    print("always execute")
+# Example4 : Raising our own exceptions
+def enterage(num):
+    if num<0:
+        raise ValueError("Only Integers are allowed")
+    if num%2==0:
+        print("even")
+    else:
+        print("odd")
+        
+print("checking number is even or off by calling function..")
+num=-1
+try:
+    enterage(num)
+except ValueError:
+    print("value error exception occured and handled...")
+enterage(-1)
+print("program completed..")
+
